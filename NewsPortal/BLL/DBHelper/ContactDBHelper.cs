91364e59a14e23cd24fc48b6d183038c6a1edb9f
Pritimay Ghosh <pritimay9.1.93@live.com>
2022-07-29 11:50:14 +0530

    first commit

---
+++ NewsPortal/BLL/DBHelper/ContactDBHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsPortal.BLL.DBHelper
+{
+    public class ContactDBHelper
+    {
+        public void InstertContact()
+        {
+
+        }
+    }
+}
